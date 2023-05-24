0d1cf52ae21ef1e2e0f21478c8f0985ca366c570
MohdNadeem <codewithnadeem@gmail.com>
2023-05-25 01:48:11 +0530

    added a typing effect to subtitle

---
+++ website/typing.js
@@ -0,0 +1,19 @@
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.zml6 .zletters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });

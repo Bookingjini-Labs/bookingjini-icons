b5c4dfe2315e7eb850a697e1b518a6b9123c87a7
Harmeet Singh <130365071+harmeetsingh11@users.noreply.github.com>
2023-07-06 15:08:18 +0000

    fix: bug on browser's console

--- website/script.js
+++ website/script.js
@@ -102,36 +102,14 @@ function searchIcon() {
   
     const darkModeToggle = document.querySelector("#darkmode-toggle");
     const theme = document.querySelector("#theme-link");
-
     darkModeToggle.addEventListener("change", function () {
       if (darkModeToggle.checked) {
         theme.href = "style-dark.css";
-      } else {
+      } 
+      else {
         theme.href = "style.css";
       }
     });
 
 
- // Function for Enabling Light Mode 
-
-    const btn = document.querySelector(".btn-toggle");
-    const footer = document.getElementById("custom-footer")
-
-    btn.addEventListener("click", function () {
-      // Swap out the URL for the different stylesheets
-      if (theme.getAttribute("href") == "style.css") {
-        theme.href = "style-dark.css";
-        btn.textContent = "Light Mode";
-
-
-      } else {
-        theme.href = "style.css";
-        btn.textContent = "Dark Mode";
-      }
-
-      footer.classList.toggle("dark-color")
-      footer.classList.toggle("light-color")
-
-
-    });
 
